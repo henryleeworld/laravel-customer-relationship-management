2141e2ac95a903abc002e202530131ae26ccb84b
Henry Lee <henryleeworld@gmail.com>
2024-05-07 00:36:35 +0800

    feat:更新最新資料

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.user')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-6">
-        <div class="card mx-4">
-            <div class="card-body p-4">
-                {{ __('You are logged in!') }}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card mx-4">
+                <div class="card-body p-4">
+                    {{ __('You are logged in!') }}
+                </div>
             </div>
         </div>
     </div>
